244f31d2c60b24d8d8bfbc58aae2f712371e5d04
SanuSamanta-oss <changeislaw777@gmail.com>
2026-07-15 19:36:51 +0530

    modification in q_input

--- Q_input.java
+++ Q_input.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class Q_input {
     public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter your second number:");
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.printf("Sum of %d and %d is :", a , b , sum);
-        System.out.println(sum);
-
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your first number:");
+            int a = sc.nextInt();
+            System.out.println("Enter your second number:");
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.printf("Sum of %d and %d is :", a , b , sum);
+            System.out.println(sum);
+        }
     }
     
 }
