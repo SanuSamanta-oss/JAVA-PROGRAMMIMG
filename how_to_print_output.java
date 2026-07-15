4d1832b71fb3bc886d6e2b3cfe4ae4bc8c4e1c2d
SanuSamanta-oss <changeislaw777@gmail.com>
2026-07-15 19:23:09 +0530

    variable

---
+++ how_to_print_output.java
@@ -0,0 +1,14 @@
+public class how_to_print_output {
+    public static void main(String args[]) {
+        //output 
+        //System[class].out.println()[function];
+
+        
+        System.out.println("Hello World");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*\n**\n***\n****\n");
+    }
+}
---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String args[]){
+        //variables
+
+        String name = "Tony Stark";
+        double price = 25.25;
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+        b=20;
+        a=7;
+        int mul = a * b;
+        System.out.println(mul);
+        
+        
+    }
+    
+}
