1975fca71f0aad153e346b9aeca563d49bbbb2ee
SanuSamanta-oss <changeislaw777@gmail.com>
2026-07-15 19:37:55 +0530

    modification in variable

--- variable.java
+++ variable.java
@@ -3,7 +3,9 @@ public class variable {
         //variables
 
         String name = "Tony Stark";
+        System.out.println(name);
         double price = 25.25;
+        System.out.println(price);
         int a = 25;
         int b = 10;
         int sum = a + b;
