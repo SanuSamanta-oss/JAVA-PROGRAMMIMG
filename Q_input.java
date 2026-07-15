8c60c607f922d685c6723ed0ff4850b57404297b
SanuSamanta-oss <changeislaw777@gmail.com>
2026-07-15 19:26:01 +0530

    questions on input  taking

---
+++ Q_input.java
@@ -0,0 +1,18 @@
+//question: find sum of two input integer numbers
+
+import java.util.*;
+
+public class Q_input {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter your second number:");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.printf("Sum of %d and %d is :", a , b , sum);
+        System.out.println(sum);
+
+    }
+    
+}
